eb39b878378e2dce742ed3eb5d165697e6db82a8
leopham <hongthaipro@gmail.com>
2024-08-22 10:19:52 +0700

    add test duplicate vote

--- src/vote.rs
+++ src/vote.rs
@@ -69,4 +69,37 @@ mod tests {
         let no = vote.get_vote(alice, bob);
         assert_eq!(no, false);
     }
+
+    #[test]
+    fn test_duplicate_vote_should_not_work() {
+        let alice = 1u64;
+        let bob = 2u64;
+        let mut vote = VotePallet::<Runtime>::new();
+
+        // alice vote cho bob
+
+        let result = vote.vote(alice, bob);
+        assert!(result.is_ok());
+
+        // alice vote cho bob lần 2
+        let result = vote.vote(alice, bob);
+        assert!(result.is_err());
+
+        // kiểm tra vote
+        let yes = vote.get_vote(alice, bob);
+        assert_eq!(yes, true);
+
+        // alice revoke bob
+
+        let result = vote.revoke(alice, bob);
+        assert!(result.is_ok());
+
+        // alice revoke bob lần 2
+        let result = vote.revoke(alice, bob);
+        assert!(result.is_err());
+
+        // kiểm tra vote
+        let no = vote.get_vote(alice, bob);
+        assert_eq!(no, false);
+    }
 }
